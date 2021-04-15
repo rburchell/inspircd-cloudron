0b55acd03b45a5b27134f96202e6a4c9b1fe4ffb
Robin Burchell <robin.burchell@crimson.no>
2021-04-15 22:45:41 +0200

    healthcheck: log requests

--- pkg/healthcheck.php
+++ pkg/healthcheck.php
@@ -3,10 +3,12 @@ header("Server: noyb");
 header("X-Powered-By: noyb");
 
 if ($_SERVER["REQUEST_URI"] == "/") {
+        file_put_contents("php://stdout", "healthcheck.php: Responding OK to healthcheck request\n");
         header("HTTP/1.0 200 OK");
         echo "<!doctype html>";
         echo "<html><body>everything's fine, go away</body></html>\n";
 } else {
+        file_put_contents("php://stdout", "healthcheck.php: Unknown request made: {$_SERVER["REQUEST_URI"]}\n");
         header("HTTP/1.0 418 I'm a teapot!");
         echo "<!doctype html>";
         echo "<html><body>I have no idea what you want, go away</body></html>\n";
